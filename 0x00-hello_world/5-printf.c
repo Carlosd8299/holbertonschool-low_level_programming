1dccb0f6e74e6436586a08a983511b164bdc4780
carlosd8299 <carlosd8299@hotmail.com>
2020-06-10 21:14:22 +0000

    five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - starts execution
+ *
+ * Return: 0 indicates sucesfull execution.
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
