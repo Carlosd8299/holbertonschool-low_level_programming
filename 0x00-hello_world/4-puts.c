647ac64474833395aa26f3a825d7fc06357c7701
carlosd8299 <carlosd8299@hotmail.com>
2020-06-10 20:43:56 +0000

    Hi pone

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - starts execution
+ *
+ * Return: 0 indicates sucesfull execution.
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
