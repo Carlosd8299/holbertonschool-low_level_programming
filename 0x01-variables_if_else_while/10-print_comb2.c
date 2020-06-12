ea923f3d227ff8f50b0e2d759eb83f583d25b2aa
carlosd8299 <carlosd8299@hotmail.com>
2020-06-12 01:09:34 +0000

    0099

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 00 to 99 in between a ',' .
+ *
+ * Return: 0 indicates sucesfull execution.
+ */
+
+int main(void)
+{
+  int i;
+  int a;
+
+for (i = 48; i <= 57; i++)
+{
+
+for (a = 48; a <= 57; a++)
+{
+
+putchar(i);
+putchar(a);
+
+if (i < 57 || a < 57)
+{
+
+putchar(44);
+putchar(32);
+
+}
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
