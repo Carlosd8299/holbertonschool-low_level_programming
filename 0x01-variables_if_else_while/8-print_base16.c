999a71f4432b822464246195ddfaf3041b6a7ce4
carlosd8299 <carlosd8299@hotmail.com>
2020-06-12 00:58:26 +0000

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to f.
+ *
+ * Return: 0 indicates sucesfull execution.
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+    {
+
+putchar(i);
+
+}
+
+for (i = 97; i <= 102; i++)
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+}
